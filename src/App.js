50b34aa8a01eef45a09971f10a6e2f3f5d2e1312
Autonoting <>
2021-09-07 15:34:00 +0000

    home work1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-					Hello World, Ract!
+					Hello World, Ract.JS!
         </p>
       </header>
     </div>
